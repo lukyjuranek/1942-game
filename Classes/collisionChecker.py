1907876e77acfdc8dffc5b76c1ebb4ad2028e3df
lukyjuranek <lukasjuranek382@gmail.com>
2022-12-01 01:38:21 +0100

    Change checkCollision to a classmethod

--- Classes/collisionChecker.py
+++ Classes/collisionChecker.py
@@ -1,5 +1,6 @@
 class CollisionChecker:
-    def checkCollision(ob1: object, ob2: object):
+    @classmethod # This is a class method meaning that it can be called without creating an instance of the class
+    def checkCollision(cls, ob1, ob2):
         '''Checks if the ob1(object1) and ob2(object2) are overlapping'''
         if ob1.x < ob2.x + ob2.width and ob1.x + ob1.width > ob2.x and ob1.y < ob2.y + ob2.height and ob1.height + ob1.y > ob2.y:
             return True
